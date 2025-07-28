6d5e6c651f4e19b9bbb91b1d82d1450e7fb4adf2
hayla-mcheik <117450554+hayla-mcheik@users.noreply.github.com>
2025-07-28 13:50:56 +0300

    Initial commit

--- app/Models/Technologies.php
+++ app/Models/Technologies.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Technologies extends Model
 {
         protected $table='technologies';
-    protected $fillable=['title','description','image','color'];
+    protected $fillable=['title','description','image','color','links'];
 }
